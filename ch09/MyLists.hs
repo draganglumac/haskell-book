b044ba928051543e55da56494282db3a68245b9a
Dragan Glumac <dragan.glumac@gmail.com>
2017-05-15 21:36:32 +0100

    Map

--- ch09/MyLists.hs
+++ ch09/MyLists.hs
@@ -42,3 +42,19 @@ wordz s
 -- comprehensions
 squaredCubes :: Integral a => [(a, a)]
 squaredCubes = [(x^2, y^3) | x <- [1..5], y <- [1..5], x^2 < 50, y^3 < 50]
+
+-- myMap and myReverse tail-recursive, purely accademic exercise as this post clearly explains
+-- https://softwareengineering.stackexchange.com/questions/323270/implementing-map-with-tail-recursion
+myMapTail :: (a -> b) -> [a] -> [b]
+myMapTail f = go [] where
+  go acc [] = myReverse acc
+  go acc (x : xs) = go ((f x) : acc) xs
+
+myReverseTail :: [a] -> [a]
+myReverseTail = go [] where
+  go acc [] = acc
+  go acc (x : xs) = go (x : acc) xs
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap _ []       = []
+myMap f (x : xs) = f x : myMap f xs
