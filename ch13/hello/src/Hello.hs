21a6cff61c74d2f99a757796b4cd3b37d7a2e919
Dragan Glumac <dragan.glumac@gmail.com>
2017-05-21 21:01:51 +0100

    Exporting bindings from a module explicitly.

--- ch13/hello/src/Hello.hs
+++ ch13/hello/src/Hello.hs
@@ -1,6 +1,12 @@
-module Hello where
+-- the following will export all top leve bindings
+-- module Hello where
+--
+-- but, we want to show how to explicity export selected bindings
+-- spaces next to parentheses seem to be required [DG]
+module Hello
+  ( sayHello )
+  where
 
 sayHello  :: IO ()
 sayHello = do
   putStrLn "hello world"
-
