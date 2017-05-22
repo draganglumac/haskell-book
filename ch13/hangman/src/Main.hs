f730fb06c2c8ca29204e9538e31630f0dbd60fc6
Dragan Glumac <dragan.glumac@gmail.com>
2017-05-22 13:39:33 +0100

    A bit more work on hangman.

--- ch13/hangman/src/Main.hs
+++ ch13/hangman/src/Main.hs
@@ -9,5 +9,47 @@ import Data.List (intersperse)
 import System.Exit (exitSuccess)
 import System.Random (randomRIO)
 
+type WordList = [String]
+
+allWords :: IO WordList
+allWords = do
+  dict <- readFile "data/dict.txt"
+  return (lines dict)
+
+minWordLength :: Int
+minWordLength = 5
+
+maxWordLength :: Int
+maxWordLength = 9
+
+gameWords :: IO WordList
+gameWords = do
+  aw <- allWords
+  return (filter gameLength aw)
+  where gameLength w =
+          let l = length (w :: String)
+          in l > minWordLength && l < maxWordLength
+
+randomWord :: WordList -> IO String
+randomWord wl = do
+  randomIndex <- randomRIO (0, (length wl) - 1)
+  return $ wl !! randomIndex
+
+-- binds gameWords list to randomWords generator
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
+
+data Puzzle = Puzzle String [Maybe Char] [Char]
+
+instance Show Puzzle where
+  show (Puzzle _ discovered guessed) =
+    (intersperse ' ' $ fmap renderPuzzleChar discovered)
+    ++ " Guessed so far: " ++ guessed
+
+freshPuzzle :: String -> Puzzle
+freshPuzzle word = undefined
+
+renderPuzzleChar = undefined
+
 main = do
   undefined
