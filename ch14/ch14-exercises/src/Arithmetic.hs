3dc288aa2e5f92fd31d941993ef7d1c6a95e77d7
Dragan Glumac <dragan.glumac@gmail.com>
2017-05-25 20:43:36 +0100

    Cracked it! Need to use forAll to pass in the generator.

--- ch14/ch14-exercises/src/Arithmetic.hs
+++ ch14/ch14-exercises/src/Arithmetic.hs
@@ -1,4 +1,6 @@
 module Arithmetic where
 
+import Data.List
+
 half :: Fractional a => a -> a
 half a = a / 2
--- ch14/ch14-exercises/test/ArithmeticTests.hs
+++ ch14/ch14-exercises/test/ArithmeticTests.hs
@@ -2,10 +2,36 @@ module ArithmeticTests where
 
 import Arithmetic
 import Test.QuickCheck
+import Data.List (sort)
 
 halfIdentity :: Double -> Bool
 halfIdentity i = ((*2) . half $ i) == i
 
+listOrdered :: (Ord a) => [a] -> Bool
+listOrdered as = snd $ foldr go (Nothing, True) as
+  where go _ status@(_, False) = status
+        go y (Nothing, t) = (Just y, t)
+        go y (Just x, t) = (Just y, x >= y)
+
+sortedList :: (Arbitrary a, Ord a) => Gen [a]
+sortedList = do
+  as <- arbitrary
+  return (sort as)
+
+sortedIntList :: Gen [Int]
+sortedIntList = sortedList
+
+sortedStringList :: Gen [String]
+sortedStringList = sortedList
+
+listIntOrdered :: [Int] -> Bool
+listIntOrdered = listOrdered
+
+listStringOrdered :: [String] -> Bool
+listStringOrdered = listOrdered
+
 main :: IO ()
 main = do
   quickCheck halfIdentity
+  quickCheck $ forAll sortedIntList listIntOrdered
+  quickCheck $ forAll sortedStringList listStringOrdered
