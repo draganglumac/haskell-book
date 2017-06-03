a67534161e1e4913d4497f3a0fb0560468008a52
Dragan Glumac <dragan.glumac@gmail.com>
2017-06-03 21:55:50 +0100

    A bit of theory in the comments.

--- ch17/applicative/src/Lib.hs
+++ ch17/applicative/src/Lib.hs
@@ -4,3 +4,47 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+{-
+  Functor vs Applicative
+  ----------------------
+
+  class Functor a => Applicative a where
+    pure :: a -> f a
+    (<*>) :: f (a -> b) -> f a -> f b
+
+    fmap  ::    a -> b  -> f a -> f b
+
+  Difference is small, we have an f in front of (a -> b), but the increase in
+  power it introduces is profound. For one thing, any Applicative also has a
+  Functor and not merely by definition -- you can define a Functor in terms
+  of provided Applicative instance.
+
+    fmap f x = pure f <*> x
+
+  How do Monoid-ness and Functor-ness combine for tuple
+
+  instance (Monoid a, Monoid b) => Monoid (a,b) where
+    mempty = (mempty, mempty)
+    (a, b) `mappend` (a',b') = (a `mappend` a', b `mappend` b')
+
+  instance Monoid a => Applicative ((,) a) where
+    pure x = (mempty, x)
+    (u,f) <*> (v,x) = (u `mappend` v, f x)
+
+  so for the first element in the tuple combination is done by mappend
+  and it needs to be Monoid, but for the second element we combine using
+  the function f.
+
+  instance Monoid a => Monoid (Maybe a) where
+    mempty = Nothing
+    mappend m Nothing = m
+    mappend Nothing m = m
+    mappend (Just a) (Just b) = Just (a `mappend` b)
+
+  instance Applicative Maybe where
+    pure = Just
+    Nothing <*> _ = Nothing
+    _ <*> Nothing = Nothing
+    Just f <*> Just a = Just (f a)
+-}
