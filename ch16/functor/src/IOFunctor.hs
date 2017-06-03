e5437d9ea0e47851c1f778862133b7a754b97605
Dragan Glumac <dragan.glumac@gmail.com>
2017-06-03 11:23:37 +0100

    Forgot this last time.

--- ch16/functor/src/IOFunctor.hs
+++ ch16/functor/src/IOFunctor.hs
@@ -5,3 +5,11 @@ module IOFunctor where
 
 getInt :: IO Int
 getInt = fmap read getLine
+
+{-
+    A way you can read getLine here is that it's not a String, but rather
+  a way to obtain a String.
+    IO does not guarantee that effects will be performed, but it does mean
+  that they could be performed. Here the side effect needs to block and wait
+  for user input via the standard input stream the operating system provides.
+-}
