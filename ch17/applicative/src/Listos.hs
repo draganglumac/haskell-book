fa0a7f17a0beb146fef309100b9fae0bb449ff42
Dragan Glumac <dragan.glumac@gmail.com>
2017-06-06 11:03:44 +0100

    A few refactorings.

--- ch17/applicative/src/Listos.hs
+++ ch17/applicative/src/Listos.hs
@@ -1,5 +1,7 @@
 module Listos where
 
+import Data.Monoid
+
 data List a = Nil | Cons a (List a) deriving (Eq, Show)
 
 instance Monoid (List a) where
@@ -18,7 +20,7 @@ instance Applicative List where
   -- apply
   (<*>) _ Nil = Nil
   (<*>) Nil _ = Nil
-  (<*>) (Cons f fs) as = fmap f as `mappend` (fs <*> as)
+  (<*>) (Cons f fs) as = fmap f as <> (fs <*> as)
 
 take' :: Int -> List a -> List a
 take' _ Nil = Nil
@@ -29,8 +31,22 @@ take' n (Cons a as) = if n > 0
 newtype ZipList' a = ZipList' (List a) deriving (Eq, Show)
 
 instance Functor ZipList' where
-  fmap f (ZipList' xs) = ZipList' $ fmap f xs
+  fmap f (ZipList' xs) = ZipList' $ f <$> xs
 
 instance Applicative ZipList' where
   pure a = ZipList' (pure a)
   ZipList' fs <*> ZipList' as = ZipList' (fs <*> as)
+
+append :: List a -> List a -> List a
+append Nil ys = ys
+append (Cons x xs) ys = Cons x $ xs `append` ys
+
+fold :: (a -> b -> b) -> b -> List a -> b
+fold _ b Nil = b
+fold f b (Cons h t) = f h (fold f b t)
+
+concat' :: List (List a) -> List a
+concat' = fold append Nil
+
+flatMap :: (a -> List b) -> List a -> List b
+flatMap f as = concat' $ fmap f as -- or f <$> as if you prefer
