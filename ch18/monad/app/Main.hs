ef1fd3e10701d510362622b29a0838521b8893be
Dragan Glumac <dragan.glumac@gmail.com>
2017-06-07 16:50:50 +0100

    More funky goodness of bind and sequence vs do.

--- ch18/monad/app/Main.hs
+++ ch18/monad/app/Main.hs
@@ -2,6 +2,17 @@ module Main where
 
 import Playground
 
+sequenceActions :: (IO (), IO ())
+sequenceActions = (first, second) where
+  first = putStrLn "First"
+  second = putStrLn "Second"
+
 main :: IO ()
 main = do
   honkIfYouLikeBind
+  fst sequenceActions
+  snd sequenceActions
+  (fst sequenceActions) >> (snd sequenceActions)
+  (fst sequenceActions) *> (snd sequenceActions)
+  putStrLn "badoomBadoom"
+  badoomBadoom
--- ch18/monad/src/Playground.hs
+++ ch18/monad/src/Playground.hs
@@ -15,3 +15,65 @@ honkIfYouLikeBind = do
   print $ bind addOne [1..10]
   putStrLn "[1..10] >>= addOne"
   print $ [1..10] >>= addOne
+
+sequencing :: IO ()
+sequencing = do
+  putStrLn "Hello"
+  putStrLn "World"
+
+sequencing' :: IO ()
+sequencing' =
+  putStrLn "Hello" *> putStrLn "World"
+
+sequencing'' :: IO ()
+sequencing'' =
+  putStrLn "Hello" >> putStrLn "World"
+
+binding :: IO ()
+binding = do
+  print "Your name: "
+  name <- getLine
+  putStrLn $ "Hello, " ++ name
+
+binding' :: IO ()
+binding' = print "Your name: " >> getLine >>= \name -> putStrLn $ "Hello, " ++ name
+
+badoomBadoom :: IO ()
+badoomBadoom = do
+  sequencing
+  sequencing'
+  sequencing''
+  binding
+  binding'
+  bindingAndSequencing
+  bindingAndSequencing'
+  twoBinds
+  twoBinds'
+
+bindingAndSequencing :: IO ()
+bindingAndSequencing = do
+  putStrLn "name pls:"
+  name <- getLine
+  putStrLn ("y halo thar: " ++ name)
+
+bindingAndSequencing' :: IO ()
+bindingAndSequencing' =
+  putStrLn "name pls: " >>
+  getLine >>=
+  \name -> putStrLn ("y halo thar: " ++ name)
+
+twoBinds :: IO ()
+twoBinds = do
+  putStrLn "name pls:"
+  name <- getLine
+  putStrLn "age pls:"
+  age <- getLine
+  putStrLn ("y halo thr: " ++ name ++ " who is: " ++ age ++ " yrs old.")
+
+twoBinds' :: IO ()
+twoBinds' =
+  putStrLn "name pls:" >>
+  getLine >>=
+  \name -> putStrLn "age pls:" >>
+  getLine >>=
+  \age -> putStrLn ("y halo thr: " ++ name ++ " who is: " ++ age ++ " yrs old.")
