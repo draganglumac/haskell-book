4c17af71a804fc1d3fe63addb7d0861ee8c35bd7
Dragan Glumac <dragan.glumac@gmail.com>
2017-06-30 22:02:05 +0100

    Applicative for Reader.

--- ch22/reader/src/Lib.hs
+++ ch22/reader/src/Lib.hs
@@ -37,6 +37,3 @@ pure :: a -> (r -> a)
 
 myLiftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
 myLiftA2 f x y = f <$> x <*> y
-
-asks :: (r -> a) -> Reader r a
-asks f = Reader f
--- ch22/reader/src/Reader.hs
+++ ch22/reader/src/Reader.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Reader where
 
 newtype Reader r a = Reader { runReader :: r -> a }
@@ -7,3 +9,16 @@ instance Functor (Reader r) where
 
 ask :: Reader a a
 ask = Reader id
+
+asks :: (r -> a) -> Reader r a
+asks = Reader
+
+instance Applicative (Reader r) where
+  pure :: a -> Reader r a
+  pure a = Reader $ const a
+
+  (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
+  -- when unpacked this type is
+  --(<*>) :: (r -> a -> b) -> (r -> a) -> (r -> b)
+  -- so just use a lambda to make the first argument explicit
+  (Reader rab) <*> (Reader ra) = Reader $ \r -> rab r (ra r)
