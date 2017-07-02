94d8e5e55387c835b963217a7c47031aa5094005
Dragan Glumac <dragan.glumac@gmail.com>
2017-07-02 18:12:00 +0100

    Reader Monad implementation.

--- ch22/reader/src/MonkeyAbout.hs
+++ ch22/reader/src/MonkeyAbout.hs
@@ -36,10 +36,42 @@ badumDoosh :: IO ()
 badumDoosh = do
   putStrLn ("    boop 3 = " ++ show (boop 3))
   putStrLn ("    doop 3 = " ++ show (doop 3))
-  putStrLn ("Composition and Functorial contexts: ")
+  putStrLn "Composition and Functorial contexts: "
   putStrLn ("     bip 3 = " ++ show (bip 3))
   putStrLn ("   bloop 3 = " ++ show (bloop 3))
-  putStrLn ("Applicative and Monadic contexts: ")
+  putStrLn "Applicative and Monadic contexts: "
   putStrLn ("    bbop 3 = " ++ show (bbop 3))
   putStrLn ("   duwop 3 = " ++ show (duwop 3))
   putStrLn ("boopDoop 3 = " ++ show (boopDoop 3))
+
+
+foo :: (Functor f, Num a) => f a -> f a
+foo = fmap (+1)
+
+bar :: Foldable f => t -> f a -> (t, Int)
+bar r t = (r, length t)
+
+froot :: Num a => [a] -> ([a], Int)
+froot r = (map (+1) r, length r)
+
+barOne :: Foldable t => t a -> (t a, Int)
+barOne r = (r, length r)
+
+barPlus r =(foo r, length r)
+
+frooty' :: Num a => [a] -> ([a], Int)
+frooty' r = bar (foo r) r
+
+-- make it look more Reader'y
+frooty :: Num a => [a] -> ([a], Int)
+frooty = \r -> bar (foo r) r
+
+-- but recall Monad's bind type
+-- (>==) :: Monad m =>
+--          m    a  -> a ->  m    b ->   m    b
+-- so just like in Applicative, ((->) r) is our Monad structure
+fooBind :: (r -> a) -> (a -> r -> b) -> (r -> b)
+fooBind m k = \r -> k (m r) r
+
+monaDoosh :: IO ()
+monaDoosh = undefined
--- ch22/reader/src/Reader.hs
+++ ch22/reader/src/Reader.hs
@@ -22,3 +22,19 @@ instance Applicative (Reader r) where
   --(<*>) :: (r -> a -> b) -> (r -> a) -> (r -> b)
   -- so just use a lambda to make the first argument explicit
   (Reader rab) <*> (Reader ra) = Reader $ \r -> rab r (ra r)
+
+{-
+  (>>=) :: Monad m =>      m a -> (a ->      m b) ->      m b
+  (>>=) ::            (->) r a -> (a -> (->) r b) -> (->) r b
+        ::            (r -> a) ->   (a -> r -> b) -> (r -> b)
+
+  return :: Monad m => a -> m a
+  return ::            a -> (->) r a
+         ::            a -> (r -> a)
+-}
+
+instance Monad (Reader r) where
+  return = pure
+
+  (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
+  (Reader ra) >>= aRb = Reader $ \r -> runReader (aRb $ ra r) r
