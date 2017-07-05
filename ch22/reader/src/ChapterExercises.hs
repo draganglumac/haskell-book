8c43ba84f859b00142824efb7e5448516fd8c39d
Dragan Glumac <dragan.glumac@gmail.com>
2017-07-05 17:12:44 +0100

    Chapter exercises.

---
+++ ch22/reader/src/ChapterExercises.hs
@@ -0,0 +1,38 @@
+module ChapterExercises where
+
+import Control.Applicative
+import Data.Maybe
+
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = [7, 8, 9]
+
+--lookup :: Eq a => a -> [(a, b)] -> Maybe b
+
+xs :: Maybe Integer
+xs = lookup 3 $ zip x y
+
+ys :: Maybe Integer
+ys = lookup 6 $ zip y z
+
+-- it's also nice to have one that
+-- will return Nothing, like this one
+--zip x and y using 4 as the lookup key
+zs :: Maybe Integer
+zs = lookup 4 $ zip x y
+
+-- now zip x and z using a variable lookup key
+z' :: Integer -> Maybe Integer
+z' n = lookup n $ zip x z
+
+x1 :: Maybe (Integer, Integer)
+x1 = (,) <$> xs <*> ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = (,) <$> ys <*> zs
+
+x3 :: Integer -> (Maybe Integer, Maybe Integer)
+x3 n = ((z' n), (z' n))
+
+summed :: Num c => (c, c) -> c
+summed xy = uncurry (+) xy
