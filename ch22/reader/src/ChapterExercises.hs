3253cceeb0fd6c2579c8fe05a97de6e0446bb9ac
Dragan Glumac <dragan.glumac@gmail.com>
2017-07-06 09:29:23 +0100

    Warm up exercises done.

--- ch22/reader/src/ChapterExercises.hs
+++ ch22/reader/src/ChapterExercises.hs
@@ -36,3 +36,30 @@ x3 n = ((z' n), (z' n))
 
 summed :: Num c => (c, c) -> c
 summed xy = uncurry (+) xy
+
+bolt :: Integer -> Bool
+bolt = (&&) <$> (>3) <*> (<8)
+
+-- fromMaybe :: a -> Maybe a -> a
+-- fromMaybe d Nothing = d
+-- fromMaybe d (Just x) = x
+
+sequA :: Integral a => a -> [Bool]
+sequA m = sequenceA [(>3), (<8), even] m
+
+s' = summed <$> ((,) <$> xs <*> ys)
+
+main :: IO ()
+main = do
+  print $ sequenceA [Just 3, Just 2, Just 1]
+  print $ sequenceA [x, y]
+  print $ sequenceA [xs, ys]
+  print $ summed <$> ((,) <$> xs <*> ys)
+  print $ fmap summed ((,) <$> xs <*> zs)
+  print $ bolt 7
+  print $ fmap bolt z
+  print $ sequenceA [(>3), (<8), even] 7
+  print $ foldr (&&) True (sequA 3)
+  print $ foldr (&&) True (sequA 6)
+  print $ sequA $ fromMaybe 0 s'
+  print $ bolt $ fromMaybe 0 ys
