4ff2a9ff014202d92e236e68c7a96c0941ea08d4
Dragan Glumac <dragan.glumac@gmail.com>
2017-07-07 10:05:13 +0100

    Die is cast.

---
+++ ch23/state/src/Die.hs
@@ -0,0 +1,22 @@
+module Die where
+
+-- Six-sided die
+data Die =
+    DieOne
+  | DieTwo
+  | DieThree
+  | DieFour
+  | DieFive
+  | DieSix
+  deriving (Eq, Show)
+
+intToDie :: Int -> Die
+intToDie n = case n of
+               1 -> DieOne
+               2 -> DieTwo
+               3 -> DieThree
+               4 -> DieFour
+               5 -> DieFive
+               6 -> DieSix
+               -- Use this tactic _extremely_ sparingly
+               x -> error $ "intToDie got non 1-6 integer: " ++ show x
---
+++ ch23/state/src/Gauntlet.hs
@@ -0,0 +1,6 @@
+module Gauntlet where
+
+import Control.Applicative (liftA3)
+import Control.Monad (replicateM)
+import Control.Monad.Trans.State
+import System.Random
